07cf823e855b4fae03c7bbdc7e3403958c93afd2
mattisonchao <mattisonchao@gmail.com>
2026-08-28 19:37:48 +0800

    refactor: establish cata and func modules

---
+++ lyrad/cata/src/lib.rs
@@ -0,0 +1,4 @@
+//! Query and cluster control plane for Lyra.
+//!
+//! Cata parses and plans queries, maintains the cluster view through Oxia,
+//! and distributes execution tasks to Func runtimes.
---
+++ lyrad/func/src/lib.rs
@@ -0,0 +1,4 @@
+//! Query task runtime for Lyra.
+//!
+//! Func executes distributed query fragments and can participate in a plan as
+//! a source, transformation, exchange, or sink.
