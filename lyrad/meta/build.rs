03d1df00c42da902c79ede5d9dfced49d47731ad
mattisonchao <mattisonchao@gmail.com>
2026-09-06 20:44:32 +0800

    chore(meta): remove unused storage protocol

--- lyrad/meta/build.rs
+++ lyrad/meta/build.rs
@@ -7,11 +7,7 @@ fn main() {
         .build_server(true)
         .compile_protos_with_config(
             config,
-            &[
-                "proto/pb_external.proto",
-                "proto/pb_storage.proto",
-                "proto/pb_catalog.proto",
-            ],
+            &["proto/pb_external.proto", "proto/pb_catalog.proto"],
             &["proto"],
         )
         .unwrap();
--- lyrad/meta/src/proto/mod.rs
+++ lyrad/meta/src/proto/mod.rs
@@ -1,3 +1,3 @@
 mod generated;
 
-pub use generated::{pb_catalog, pb_ext, pb_storage};
+pub use generated::{pb_catalog, pb_ext};
